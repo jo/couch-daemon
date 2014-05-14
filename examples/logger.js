23603f08623233c59e8d63623a6562e3d026ac8c
Johannes J. Schmidt <schmidt@netzmerk.com>
2014-05-14 17:06:17 +0200

    pass url and options to  worker

--- examples/logger.js
+++ examples/logger.js
@@ -9,27 +9,29 @@
 var daemon = require('..');
 
 
-daemon(function(source) {
-  var count = 0;
+daemon(function(couch, options) {
+  return function(source) {
+    var count = 0;
 
-  return source
-    .map(function(data) {
-      if (data.stream === 'dbs') {
-        return {
-          type: 'log',
-          message: 'listening on: ' + data.db_name
-        };
-      }
-      if (data.stream === 'changes') {
-        count++;
-        return {
-          type: 'log',
-          message: 'processing (' + count + '): ' + data.db_name + '/' + data.id + '@' + data.seq,
-          db_name: data.db_name,
-          seq: data.seq
-        };
-      }
-      return data;
-    });
+    return source
+      .map(function(data) {
+        if (data.stream === 'dbs') {
+          return {
+            type: 'log',
+            message: 'listening on: ' + data.db_name
+          };
+        }
+        if (data.stream === 'changes') {
+          count++;
+          return {
+            type: 'log',
+            message: 'processing (' + count + '): ' + data.db_name + '/' + data.id + '@' + data.seq,
+            db_name: data.db_name,
+            seq: data.seq
+          };
+        }
+        return data;
+      });
+  };
 });
 
