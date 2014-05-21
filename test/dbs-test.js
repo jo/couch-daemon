de1349fed69d7ae6d2ce9a3c3e368a3aeeb8b677
Johannes J. Schmidt <schmidt@netzmerk.com>
2014-05-21 17:48:48 +0200

    WIP Support _db_updates
    1. Waiting for a follow release.
    2. Then I'll update follow on nano.
    3. Wait for nano release.
    4. Update nano here.
    5. Done.

--- test/dbs-test.js
+++ test/dbs-test.js
@@ -2,20 +2,30 @@ var helper = require('./helper');
 var dbs = require('../lib/dbs');
 
 helper.withDB('initial db', function(couch, db, t, done) {
-  dbs(couch.config.url)
-    .filter(function(d) {
-      return d.db_name === db.config.db || d.db_name === '_users';
+  var stream = dbs(couch.config.url);
+
+  setTimeout(function() {
+    stream.end();
+  }, 300);
+
+  stream
+    .map(function(d) {
+      return d.db_name;
     })
-    .take(2)
     .toArray(function(rows) {
-      t.equal(rows.length, 2, 'two rows included');
+      t.ok(rows.indexOf('_users') > -1, '_users db included');
       done();
     });
 });
 
 helper.withDB('whitelist', function(couch, db, t, done) {
-  dbs(couch.config.url, { whitelist: [db.config.db] })
-    .take(1)
+  var stream = dbs(couch.config.url, { whitelist: [db.config.db] });
+
+  setTimeout(function() {
+    stream.end();
+  }, 300);
+
+  stream
     .toArray(function(rows) {
       t.equal(rows.length, 1, 'one row included');
       t.equal(rows[0].stream, 'dbs', 'stream is dbs');
@@ -26,13 +36,18 @@ helper.withDB('whitelist', function(couch, db, t, done) {
 });
 
 helper.withDB('blacklist', function(couch, db, t, done) {
-  dbs(couch.config.url, { blacklist: ['_users'] })
-    .take(1)
-    .filter(function(d) {
-      return d.db_name === '_users';
+  var stream = dbs(couch.config.url, { blacklist: ['_users'] });
+
+  setTimeout(function() {
+    stream.end();
+  }, 300);
+
+  stream
+    .map(function(d) {
+      return d.db_name;
     })
     .toArray(function(rows) {
-      t.equal(rows.length, 0, 'no row included');
+      t.equal(rows.indexOf('_users'), -1, '_users not included');
       done();
     });
 });
